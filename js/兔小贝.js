5edc02eef54063f3d9499ba0a66934af1fd95618
tianze889 <15915343658@139.com>
2026-03-31 08:45:35 +0800

    Add files via upload

---
+++ js/兔小贝.js
@@ -0,0 +1,26 @@
+var rule = {
+    title'兔小贝',
+    host'httpswww.tuxiaobei.com',
+    homeUrl'',
+    url'listmip-datatypeId=fyclass&page=fypage&callback=',
+    detailUrl'playfyid',
+    searchUrl'searchindexkey=',
+    searchable2,
+    headers{
+        'User-Agent''MOBILE_UA'
+    },
+    timeout5000,
+    class_url'2&3&4&25',
+    class_name'儿歌&故事&国学&启蒙',
+    class_name'#page-viewport&&ul&&li;.text&&Text;a&&href;(.)',
+    cate_exclude'应用',
+    推荐'.pic-list.list-box;.items;.text&&Text;mip-img&&src;.all&&Text;a&&href',
+    doubletrue,
+    limit5,
+    play_parsetrue,
+    lazy'jsfetch_params.headers[user-agent] = IOS_UA;let html=fetch(input,fetch_params);let src = jsp.pdfh(html,body&&#videoWrap&&video-src);input=src;',
+     一级'jsondata.items;name;image;collect_num;category_id+video_id',
+    一级'jsondata.items;name;image;duration_string;video_id',
+    二级'',
+    搜索'.list-con&&.items;.text&&Text;mip-img&&src;.time&&Text;a&&href',
+}
